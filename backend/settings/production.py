db3050c8b9ad993c8127c79a04eda5995d0e33dd
kevinsun127 <kevinsun@usc.edu>
2022-05-07 20:41:54 -0700

    aws key stuff

--- backend/settings/production.py
+++ backend/settings/production.py
@@ -17,3 +17,17 @@ DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
 # TODO: Pictures
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
 MEDIA_URL = '/media/'
+
+import os
+from decouple import config
+
+os.environ.setdefault('AWS_ACCESS_KEY_ID', config('AWS_ACCESS_KEY_ID'))
+os.environ.setdefault('AWS_SECRET_ACCESS_KEY', config('AWS_SECRET_ACCESS_KEY'))
+os.environ.setdefault('AWS_S3_SIGNATURE_VERSION', config('AWS_S3_SIGNATURE_VERSION'))
+os.environ.setdefault('AWS_S3_REGION_NAME', config('AWS_S3_REGION_NAME'))
+os.environ.setdefault('AWS_STORAGE_BUCKET_NAME', config('AWS_STORAGE_BUCKET_NAME'))
+AWS_STORAGE_BUCKET_NAME = "mistbucket"
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_VERIFY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
