61e9ef982803d5ae819d1b2c04af8f828ca5b3dd
kevinsun127 <kevinsun@usc.edu>
2022-05-10 20:28:13 -0700

    fuck it, debug=True

--- backend/settings/production.py
+++ backend/settings/production.py
@@ -1,6 +1,6 @@
 from backend.settings.base import *
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://mist-backend.herokuapp.com']
 
@@ -21,8 +21,11 @@ MEDIA_URL = '/media/'
 import os
 from decouple import config
 
-os.environ.setdefault('AWS_ACCESS_KEY_ID', config('AWS_ACCESS_KEY_ID'))
-os.environ.setdefault('AWS_SECRET_ACCESS_KEY', config('AWS_SECRET_ACCESS_KEY'))
+# os.environ.setdefault('AWS_ACCESS_KEY_ID', config('AWS_ACCESS_KEY_ID'))
+# os.environ.setdefault('AWS_SECRET_ACCESS_KEY', config('AWS_SECRET_ACCESS_KEY'))
+SECRET_KEY = config('SECRET_KEY')
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 AWS_S3_SIGNATURE_VERSION = "s3v4"
 AWS_S3_REGION_NAME = "us-west-1"
 AWS_STORAGE_BUCKET_NAME = "mistbucket"
@@ -30,4 +33,3 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 AWS_S3_VERIFY = True
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
