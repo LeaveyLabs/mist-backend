ea6ae67e611487d78882d3db624b2622dbb785d8
kevinsun127 <kevinsun@usc.edu>
2022-05-10 20:02:30 -0700

    staticfiles test

--- backend/settings/production.py
+++ backend/settings/production.py
@@ -30,3 +30,4 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 AWS_S3_VERIFY = True
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
