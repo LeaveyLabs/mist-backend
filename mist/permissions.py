5c818ca4cc5cbf844b0a838e80a99a449c278e78
kevinsun127 <kevinsun@usc.edu>
2022-05-24 21:08:07 -0700

    get_token -> get_user from request

--- mist/permissions.py
+++ mist/permissions.py
@@ -1,127 +1,121 @@
 from rest_framework import permissions
-from rest_framework.authtoken.models import Token
-
-def get_token_from_request(request):
-    matching_tokens = Token.objects.filter(key=request.auth)
-    if not matching_tokens: return None
-    matching_token = matching_tokens[0]
-    return matching_token
+from users.generics import get_user_from_request
 
 class PostPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method == "POST":
-            matching_token = get_token_from_request(request)
-            if not matching_token: return False
-            author = request.data.get('author')
-            return matching_token.user.pk == author
+            requesting_user = get_user_from_request(request)
+            author_pk = request.data.get('author')
+            if not requesting_user: return False
+            return requesting_user.pk == author_pk
         else:
             return True
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        matching_token = get_token_from_request(request)
-        if not matching_token: return False
-        return matching_token.user == obj.author
+        requesting_user = get_user_from_request(request)
+        if not requesting_user: return False
+        return requesting_user == obj.author
 
 class CommentPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method == "POST":
-            matching_token = get_token_from_request(request)
-            if not matching_token: return False
-            author = request.data.get('author')
-            return matching_token.user.pk == author
+            requesting_user = get_user_from_request(request)
+            author_pk = request.data.get('author')
+            if not requesting_user: return False
+            return requesting_user.pk == author_pk
         else:
             return True
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        matching_token = get_token_from_request(request)
-        if not matching_token: return False
-        return matching_token.user == obj.author
+        requesting_user = get_user_from_request(request)
+        if not requesting_user: return False
+        return requesting_user == obj.author
 
 class VotePermission(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method == "POST":
-            matching_token = get_token_from_request(request)
-            if not matching_token: return False
-            voter = request.data.get('voter')
-            return matching_token.user.pk == voter
+            requesting_user = get_user_from_request(request)
+            voter_pk = request.data.get('voter')
+            if not requesting_user: return False
+            return requesting_user.pk == voter_pk
         else:
             return True
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        matching_token = get_token_from_request(request)
-        if not matching_token: return False
-        return matching_token.user == obj.voter
+        requesting_user = get_user_from_request(request)
+        if not requesting_user: return False
+        return requesting_user == obj.voter
 
 class FlagPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method == "POST":
-            matching_token = get_token_from_request(request)
-            if not matching_token: return False
-            flagger = request.data.get('flagger')
-            return matching_token.user.pk == flagger
+            requesting_user = get_user_from_request(request)
+            flagger_pk = request.data.get('flagger')
+            if not requesting_user: return False
+            return requesting_user.pk == flagger_pk
         else:
             return True
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        matching_token = get_token_from_request(request)
-        if not matching_token: return False
-        return matching_token.user == obj.flagger
+        requesting_user = get_user_from_request(request)
+        if not requesting_user: return False
+        return requesting_user == obj.flagger
 
 class TagPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method == "POST":
-            matching_token = get_token_from_request(request)
-            if not matching_token: return False
-            tagging_user = request.data.get('tagging_user')
-            return matching_token.user.pk == tagging_user
+            requesting_user = get_user_from_request(request)
+            tagging_user_pk = request.data.get('tagging_user')
+            if not requesting_user: return False
+            return requesting_user.pk == tagging_user_pk
         else:
             return True
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        matching_token = get_token_from_request(request)
-        if not matching_token: return False
-        return matching_token.user == obj.tagging_user
+        requesting_user = get_user_from_request(request)
+        if not requesting_user: return False
+        return requesting_user == obj.tagging_user
 
 class BlockPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method == "POST":
-            matching_token = get_token_from_request(request)
-            if not matching_token: return False
-            blocking_user = request.data.get('blocking_user')
-            return matching_token.user.pk == blocking_user
+            requesting_user = get_user_from_request(request)
+            blocking_user_pk = request.data.get('blocking_user')
+            if not requesting_user: return False
+            return requesting_user.pk == blocking_user_pk
         else:
             return True
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        matching_token = get_token_from_request(request)
-        if not matching_token: return False
-        return matching_token.user == obj.blocking_user
+        requesting_user = get_user_from_request(request)
+        if not requesting_user: return False
+        return requesting_user == obj.blocking_user
 
 class MessagePermission(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method == "POST":
-            matching_token = get_token_from_request(request)
-            if not matching_token: return False
-            from_user = request.data.get('from_user')
-            return matching_token.user.pk == from_user
+            requesting_user = get_user_from_request(request)
+            from_user_pk = request.data.get('from_user')
+            if not requesting_user: return False
+            return requesting_user.pk == from_user_pk
         else:
             return True
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        matching_token = get_token_from_request(request)
-        if not matching_token: return False
-        return matching_token.user == obj.from_user
+        requesting_user = get_user_from_request(request)
+        if not requesting_user: return False
+        return requesting_user == obj.from_user
