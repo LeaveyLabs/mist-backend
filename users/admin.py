b3606a0b3cd31eb3cbeaf38f6630a09a709bdbb5
kevinsun127 <kevinsun@usc.edu>
2022-05-25 22:19:04 -0700

    register email auth

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import EmailAuthentication, User
 
 class UserAdmin(UserAdmin):
     model = User
 
 admin.site.register(User, UserAdmin)
+admin.site.register(EmailAuthentication, UserAdmin)
