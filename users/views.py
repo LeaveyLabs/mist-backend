8d21dedef3536962aafa4dfaa32c317d5933596f
kevinsun127 <kevinsun@usc.edu>
2022-05-27 23:43:17 -0700

    unneccessary import

--- users/views.py
+++ users/views.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-from requests import request
 from rest_framework import viewsets, generics
 from rest_framework import status
 from rest_framework.authtoken.models import Token
