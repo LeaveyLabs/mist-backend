4ab4b07b4ee4518ffc400b908dec3f19ace18365
kevinsun127 <kevinsun@usc.edu>
2022-06-11 14:25:50 -0700

    get requires authentication

--- users/permissions.py
+++ users/permissions.py
@@ -4,6 +4,13 @@ from rest_framework.authtoken.models import Token
 from users.generics import get_user_from_request
 
 class UserPermissions(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method == "GET":
+            requesting_user = get_user_from_request(request)
+            if not requesting_user: return False
+            return True
+        return super().has_permission(request, view)
+
     def has_object_permission(self, request, view, obj):
         is_safe_method = request.method in permissions.SAFE_METHODS
         if is_safe_method: return True
