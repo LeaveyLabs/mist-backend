7d688afebd7edf340c898334d8152650aac46509
kevinsun127 <kevinsun@usc.edu>
2022-08-13 13:25:31 -0700

    admin + new static root

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import EmailAuthentication, User
+from .models import EmailAuthentication, PhoneNumberAuthentication, PhoneNumberReset, User
 
 class UserAdmin(UserAdmin):
     model = User
 
 admin.site.register(User, UserAdmin)
 admin.site.register(EmailAuthentication)
+admin.site.register(PhoneNumberAuthentication)
+admin.site.register(PhoneNumberReset)
