96b5533820a0fdaec8ebebb1204bfca4ceed7899
kevinsun127 <kevinsun@usc.edu>
2022-08-22 18:12:43 -0700

    correct application instance

--- backend/celery.py
+++ backend/celery.py
@@ -1,13 +1,15 @@
 import os
+
 from celery import Celery
 from celery.schedules import crontab
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_celery.settings")
-app = Celery("django_celery")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.environ['DJANGO_SETTINGS_MODULE'])
+
+app = Celery("backend")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
 
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
     from notifications.tasks import send_mistbox_notifications
-    sender.add_periodic_task(crontab(hour=0), send_mistbox_notifications.s())
+    sender.add_periodic_task(crontab(hour=0, minute=0), send_mistbox_notifications.s())
