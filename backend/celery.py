4a1e8bb5d78368ac45cb17896d6634f546563452
kevinsun127 <kevinsun@usc.edu>
2022-08-31 15:03:38 -0700

    every hour, tally random upvotes

--- backend/celery.py
+++ backend/celery.py
@@ -3,6 +3,8 @@ import os
 from celery import Celery
 from celery.schedules import crontab
 
+from mist_worker.tasks import tally_random_upvotes_task
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.environ['DJANGO_SETTINGS_MODULE'])
 
 app = Celery("backend")
@@ -14,3 +16,4 @@ def setup_periodic_tasks(sender, **kwargs):
     from mist_worker.tasks import send_mistbox_notifications_task, make_daily_mistboxes_task
     sender.add_periodic_task(crontab(hour=16, minute=59), make_daily_mistboxes_task.s())
     sender.add_periodic_task(crontab(hour=17, minute=0), send_mistbox_notifications_task.s())
+    sender.add_periodic_task(3600, tally_random_upvotes_task.s())
