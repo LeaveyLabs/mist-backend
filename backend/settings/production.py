9ac57af75eb3be72f693829187e22f99a5551804
kevinsun127 <kevinsun@usc.edu>
2022-08-31 22:46:46 -0700

    CERT_NONE

--- backend/settings/production.py
+++ backend/settings/production.py
@@ -41,4 +41,4 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_CUSTOM_DOMAIN = os.environ.get('AWS_S3_CUSTOM_DOMAIN')
 
 CELERY_BROKER_URL = os.environ.get("REDIS_TLS_URL") + f"?ssl_cert_reqs={ssl.CERT_NONE}"
-CELERY_RESULT_BACKEND = os.environ.get("REDIS_TLS_URL") + + f"?ssl_cert_reqs={ssl.CERT_NONE}"
+CELERY_RESULT_BACKEND = os.environ.get("REDIS_TLS_URL") + f"?ssl_cert_reqs={ssl.CERT_NONE}"
