5ecda40ec894b963e0d5129302d17bfb7d17c37b
kevinsun127 <kevinsun@usc.edu>
2022-09-08 20:43:53 -0700

    no more random vote

--- backend/celery.py
+++ backend/celery.py
@@ -11,7 +11,6 @@ app.autodiscover_tasks()
 
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
-    from mist_worker.tasks import reset_mistbox_opens_task, send_mistbox_notifications_task, tally_random_upvotes_task
+    from mist_worker.tasks import reset_mistbox_opens_task, send_mistbox_notifications_task
     sender.add_periodic_task(crontab(hour=17, minute=0), reset_mistbox_opens_task.s())
     sender.add_periodic_task(crontab(hour=17, minute=0), send_mistbox_notifications_task.s())
-    sender.add_periodic_task(3600, tally_random_upvotes_task.s())
