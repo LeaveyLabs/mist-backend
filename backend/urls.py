de9d8f4af2724d18a0081c90eaeb7f732469e02b
kevinsun127 <kevinsun@usc.edu>
2022-09-18 13:36:12 -0700

    need to remove silk urls

--- backend/urls.py
+++ backend/urls.py
@@ -109,4 +109,4 @@ urlpatterns = [
 
 if settings.DEBUG: 
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += [path('silk/', include('silk.urls', namespace='silk'))]
+    # urlpatterns += [path('silk/', include('silk.urls', namespace='silk'))]
