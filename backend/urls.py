8ad9561f29eda50a57374ca72507cbb140670039
kevinsun127 <kevinsun@usc.edu>
2022-09-18 18:27:59 -0700

    open heart surgery once more

--- backend/urls.py
+++ backend/urls.py
@@ -107,6 +107,6 @@ urlpatterns = [
     path('api/delete-mistbox-post/', DeleteMistboxPostView.as_view()),
 ]
 
-if settings.DEBUG: 
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += [path('silk/', include('silk.urls', namespace='silk'))]
+# if settings.DEBUG: 
+    # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # urlpatterns += [path('silk/', include('silk.urls', namespace='silk'))]
