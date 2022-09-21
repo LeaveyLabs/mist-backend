e37f2c032454ed35faab8add8bdc0c2c889a024a
kevinsun127 <kevinsun@usc.edu>
2022-09-20 23:11:09 -0700

    mistbox test

--- backend/celery.py
+++ backend/celery.py
@@ -14,4 +14,5 @@ def setup_periodic_tasks(sender, **kwargs):
     from mist_worker.tasks import reset_mistbox_opens_task, send_mistbox_notifications_task, schedule_make_your_day_mist_notifications_task
     sender.add_periodic_task(crontab(hour=17, minute=0), reset_mistbox_opens_task.s())
     sender.add_periodic_task(crontab(hour=17, minute=0), send_mistbox_notifications_task.s())
+    sender.add_periodic_task(60.0, send_mistbox_notifications_task.s())
     sender.add_periodic_task(crontab(day_of_week=[1, 3, 5],hour=17, minute=0), schedule_make_your_day_mist_notifications_task.s())
