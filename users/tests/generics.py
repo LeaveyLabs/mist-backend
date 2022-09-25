941ae85e26f736e813bc388ad2abd86dc7b4a25e
kevinsun127 <kevinsun@usc.edu>
2022-09-24 17:27:10 -0700

    no more phone number random test failures

--- users/tests/generics.py
+++ users/tests/generics.py
@@ -1,11 +1,21 @@
 def gen_phone():
+    from users.models import User
     import random
 
-    last = (str(random.randint(1,9998)).zfill(4))
-    while last in ['1111','2222','3333','4444','5555','6666','7777','8888']:
+    def phone_number_with_random_four_digits():
         last = (str(random.randint(1,9998)).zfill(4))
+        while last in ['1111','2222','3333','4444','5555','6666','7777','8888']:
+            last = (str(random.randint(1,9998)).zfill(4))
+        return f'+1310310{last}'
+
+    phone_number = phone_number_with_random_four_digits()
+    max_tries = 1000
+    tries = 0
+    while User.objects.filter(phone_number=phone_number).exists() and tries < max_tries:
+        phone_number = phone_number_with_random_four_digits()
+        tries += 1
         
-    return f'+1310310{last}'
+    return phone_number
 
 def create_dummy_user_and_token_given_id(id):
     from datetime import date
